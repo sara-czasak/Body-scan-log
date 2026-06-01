365e3b30bc080fbdb0d447927ef5335e8951fba5
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-06-01 10:51:10 +0200

    add connection func and adjust rest of funcs. add dummy records for testing

--- v1/database.py
+++ v1/database.py
@@ -2,8 +2,15 @@ import sqlite3
 from db_schema import CREATE_SCANS_TABLE, CREATE_BODY_PART_READING_TABLE
 
 
-def create_database():
+def get_connection():
     conn = sqlite3.connect('body_scan.db')
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn
+
+
+
+def create_database():
+    conn = get_connection()
     cursor = conn.cursor()
     cursor.execute(CREATE_SCANS_TABLE)
     cursor.execute(CREATE_BODY_PART_READING_TABLE)
@@ -11,5 +18,40 @@ def create_database():
     conn.close()
 
 
+def insert_scan(date, overall_score, notes=None):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO scans (date, overall_score, notes) VALUES (?, ?, ?)", (date, overall_score, notes))
+    conn.commit()
+    conn.close()
+
+
+def insert_body_part_reading(scan_id, body_part, score):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO body_part_reading (scan_id, body_part, score) VALUES (?, ?, ?)", (scan_id, body_part, score))
+    conn.commit()
+    conn.close()
+
+
+
 if __name__ == '__main__':
-    create_database()
+    # create_database()
+    # insert_scan("2026-05-27", 7)
+    # insert_scan("2026-05-28", 3)
+    # insert_scan("2026-05-29", 2)
+    # insert_scan("2026-05-30", 5)
+    # insert_scan("2026-05-31", 7)
+    # insert_body_part_reading(2, "jaw", 3)
+    # insert_body_part_reading(2, "shoulders", 3)
+    # insert_body_part_reading(3, "upper back", 2)
+    # insert_body_part_reading(3, "neck", 2)
+    #
+    # insert_body_part_reading(4, "upper back", 2)
+    # insert_body_part_reading(4, "jaw", 2)
+    # insert_body_part_reading(4, "stomach", 2)
+    # insert_body_part_reading(4, "shoulders", 2)
+    #
+    # insert_body_part_reading(5, "upper back", 7)
+    # insert_body_part_reading(5, "jaw", 7)
+    # insert_body_part_reading(5, "shoulders", 7)

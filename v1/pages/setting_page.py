f9a8b478dfbdc82b6d43fe67e41e0b17c101991e
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-06-01 15:08:27 +0200

    start working on lang select settings option

--- v1/pages/setting_page.py
+++ v1/pages/setting_page.py
@@ -8,7 +8,8 @@ class SettingsFrame(ctk.CTkFrame):
         self.settings_label = None
         self.language_button = None
         self.set_coping_strategies = None
-        self.back_button = None
+        self.back_to_menu_button = None
+        self.back_to_settings_button = None
         self.layout()
 
 
@@ -16,16 +17,34 @@ class SettingsFrame(ctk.CTkFrame):
         self.settings_label = ctk.CTkLabel(self, text="SETTINGS")
         self.settings_label.pack(padx=5, pady=5)
 
-        self.back_button = ctk.CTkButton(self, text="BACK", command=self.go_back)
-        self.back_button.pack(padx=5, pady=5)
+        self.back_to_menu_button = ctk.CTkButton(self, text="BACK", command=self.go_back_to_menu)
+        self.back_to_menu_button.pack(padx=5, pady=5)
 
-        self.language_button = ctk.CTkButton(self, text="Select Language")
+        self.language_button = ctk.CTkButton(self, text="Select Language", command=self.set_lang)
         self.language_button.pack(padx=5, pady=5)
 
         self.set_coping_strategies = ctk.CTkButton(self, text="Set coping strategies")
         self.set_coping_strategies.pack(padx=5, pady=5)
 
+        self.back_to_settings_button = ctk.CTkButton(self, text="BACK", command=self.go_back_to_settings)
 
-    def go_back(self):
+
+    def set_lang(self):
+        self.clear_layout()
+
+
+    def clear_layout(self):
+        for i in self.winfo_children():
+            i.pack_forget()
+        self.back_to_settings_button.pack(padx=5, pady=5)
+
+
+    def go_back_to_settings(self):
+        for i in self.winfo_children():
+            i.pack_forget()
+        self.parent.show_settings()
+
+
+    def go_back_to_menu(self):
         self.parent.hide_settings()
         self.parent.show_menu()
